8fdbceb3ad8d6812a6174d4ccf75f1d9ad0aabbe
Xavigit99 <xavier.ricarte@gmail.com>
2024-09-18 14:05:03 +0000

    lab1 refactored

--- shell/students/Xavier-Ricarte-Shell/lab3.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for f in ".txt" do
-mv $f backp_$f
-done
-
--- shell/students/Xavier-Ricarte-Shell/lab6.sh
+++ shell/students/Xavier-Ricarte-Shell/lab6.sh
@@ -4,15 +4,11 @@ LOGFILE="/var/log/postgresql_service.log"
 
 if systemctl is-active --quiet postgresql; then
     echo "PostgreSQL service is running."
+    exit
 else
     echo "PostgreSQL service is not running."
-fi
-
     sudo systemctl start postgresql
-
-if systemctl is-active --quiet postgresql; then
-     echo "$(date): PostgreSQL service started." >> "$LOGFILE"
-     echo "PostgreSQL service has been started and logged."
-else
-     echo "$(date): Failed to start PostgreSQL"
+    echo "$(date): PostgreSQL service started." >> "$LOGFILE"
+    echo "PostgreSQL service has been started and logged."
 fi
+
