ac8fb7380979e7a7df19dbb06c72663232e03400
PatNatixis <yodatheonewiththesparkle@gmail.com>
2024-09-18 14:54:56 +0100

    Manual update lab4.sh

--- shell/students/patricia-sanches/lab4.sh
+++ shell/students/patricia-sanches/lab4.sh
@@ -1,12 +1,12 @@
-#!bin/bash
+place=$(pwd) 
 
-$place =$(pwd)
-echo "What is the threshold?"
-read threshold
+echo "What is the threshold?" 
+read threshold 
 
-usage = $(df -h $place)
+usage=$(df --output=pcent "$place" | tail -n 1 | tr -d '%') 
 
-if threshold < usage
-echo "Disk usage ok. Current usage: $usage ."
-else 
-echo "Warning! Disk usage is above $threshold. Current usage:$usage" 
+if [ "$usage" -lt "$threshold" ]; 
+then echo "Disk usage ok. Current usage: $usage%." 
+
+else echo "Warning! Disk usage is above $threshold%. Current usage: $usage%."
+fi
