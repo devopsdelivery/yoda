bd46e5b223f3515c309247b00f81c675ecb21871
Helena <tsipehe1@gmail.com>
2024-09-18 11:14:31 +0000

    lab 2 complete

--- shell/students/helena-pereira/lab2.sh
+++ shell/students/helena-pereira/lab2.sh
@@ -1,8 +1,17 @@
-read numbers
 
-for number in $numbers; do
-	if [ $(($number%2)) -eq 0 ]
-		then echo "$number is even";
-		else echo "$number is odd";
+	read -p "Digite 3 numeros :" input
+
+	count=$(echo $input | wc -w)
+
+	if [ $((count)) -eq 3 ];
+		then
+			for number in $input; do
+				if [ $(($number%2)) -eq 0 ]
+					then echo "$number is even";
+					else echo "$number is odd";
+				fi
+			done
+
+		else
+			echo "put exact 3 numbers"
 	fi
-done
