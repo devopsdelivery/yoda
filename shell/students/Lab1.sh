4df5631c5c61dc35e0728556a4aa324bfd9d61ac
Luiza Nascimento Peres <luizanperes@gmail.com>
2024-09-18 11:16:48 +0100

    Lab1 Feito

---
+++ shell/students/Lab1.sh
@@ -0,0 +1,13 @@
+
+
+#! /bin/bash 
+
+echo "What is your first name?"
+read input1
+echo "What is your last name?"
+read input2
+echo "Where were you born?"
+read input3
+
+echo "[$input1] [$input2] [$input3]"
+
