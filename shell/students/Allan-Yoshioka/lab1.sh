dcb0ae9b462ac7e6ec9e1df8eb6db0557151f3c7
Allan Yoshioka <80413449+LstCenturion@users.noreply.github.com>
2024-09-18 10:47:34 +0100

    lab1

---
+++ shell/students/Allan-Yoshioka/lab1.sh
@@ -0,0 +1,14 @@
+#! /bin/sh
+
+echo "What is your first name?"
+read first
+
+echo "What is your first name?"
+read second 
+
+echo "Where were you born?"
+read born
+
+echo "Nice to meet you $first $second from $born."
+
+fi
