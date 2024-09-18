6b02e4aaf991bd12dcd6215818d5344a177956a2
root <tsipehe1@gmail.com>
2024-09-18 13:43:45 +0000

    lab4 complete

--- shell/students/helena-pereira/lab4.sh
+++ shell/students/helena-pereira/lab4.sh
@@ -1,12 +1,12 @@
+#!/bin/bash
+
 USAGE=$(df / | grep / | awk '{ print $5 }' | sed 's/%//')
 THRESHOLD=80
 
 
-echo $(( USAGE )) -gt $(( THRESHOLD ))
 
 
-
-if [ "$USAGE" -gt "$THRESHOLD" ];then
+if [ "$USAGE" -lt "$THRESHOLD" ]; then
 	echo "Disk usage ok. Current usage: $USAGE"
 else
 	echo "Warning: Disk usage is above $THRESHOLD. Current usage: $USAGE"
