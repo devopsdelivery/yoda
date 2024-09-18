fc80c1d4da177bc03d2c98a04df4326f43f6e58d
Diogo Vieira <diogo.romero.paz@gmail.com>
2024-09-18 14:21:56 +0100

    lab3 corrections

--- renamedir/backup_lab3.sh
+++ renamedir/backup_lab3.sh
@@ -13,7 +13,7 @@ directory="${script_path%/*}"
 for file in "$directory"/*; do
     if [ -f "$file" ]; then  # Check if it's a regular file
         filename=$(basename "$file")  # Get the file name
-        mv "$file" "$directory/$prefix$filename"  # Rename the file
+        mv "$file" "$prefix$filename"  # Rename the file
         echo "Renamed '$filename' to '$prefix$filename'"
     fi
 done
