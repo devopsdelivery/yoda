17e542025ba8ee64532ba84dca0141c6e63e2bc3
Shadow75757 <hprata6@gmail.com>
2024-09-18 14:17:57 +0000

    All labs DONE

--- shell/students/henrique-prata/lab7.sh
+++ shell/students/henrique-prata/lab7.sh
@@ -3,10 +3,8 @@
 URL="https://github.com/devopsdelivery/yoda"
 LOG_FILE="website_status.log"
 
-# Check the website status
 response=$(curl --write-out "%{http_code}" --silent --output /dev/null "$URL")
 
-# Log the result with timestamp
 if [ "$response" -eq 200 ]; then
     echo "$(date) - Website $URL is up (Status code: $response)" >> "$LOG_FILE"
 else
