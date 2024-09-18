302a6eb110cd0e7202e415c6a2940d2b4b599f47
BeautifulSopa <gnccb07@gmail.com>
2024-09-18 13:44:38 +0100

    End LAB2 Shell-Goncalo.sh

--- linux/students/Shell-Goncalo.sh
+++ linux/students/Shell-Goncalo.sh
@@ -15,3 +15,32 @@ What is your first name?Goncalo
 What is your last name?Batista
 Where were you born?Portugal
 Nice to meet you Goncalo Batista from Portugal!
+
+
+## Bash Scripting LAB2
+
+ #!/bin/sh
+
+
+if [ $# -ne 3 ]; then
+        echo "Error"
+        exit 1
+fi
+
+for i in "$@"
+do
+        if [ $((i%2)) -eq 0 ]; then
+        Even_or_Odd_numb="even"
+        else
+        Even_or_Odd_numb="odd"
+        fi
+
+        echo "the number $i is $Even_or_Odd_numb"
+done
+
+Output:
+goncalo@DESKTOP-0HE064V:~/scriptstest$ ./shellexec2.sh 1 2 3
+the number 1 is odd
+the number 2 is even
+the number 3 is odd
+
