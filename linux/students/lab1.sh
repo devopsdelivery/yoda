aa9394701cb93af754b4c97d9f706d6a77e30987
Jhonata <jhsocial@icloud.com>
2024-09-18 10:05:06 +0100

    first shell commit

---
+++ linux/students/lab1.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+# Shebang that tells the system to use bash to interpret the script
+
+## Exercises
+# - What is your first name?
+# - What is your last name?
+# - Where were you born?
+
+# Text formatting (bold and red)
+set -x # Enable debugging
+BOLD=$(tput bold)
+RED=$(tput setaf 1)
+RESET=$(tput sgr0)
+
+read -p "What's your favorite fruit? " fruit
+
+echo -n $BOLD
+case $fruit in
+   (apple) echo "An apple a day keeps the doctor away.";;
+  (banana) echo "Bananas are a good source of potassium.";;
+  (cherry) echo "Cherries are a good source of antioxidants.";;
+  (orange) echo "Oranges are a good source of vitamin C.";;
+  (*)echo "I don't like $RED$fruit$RESET.";;
+esac
+echo -n $RESET
+
+set +x # Disable debugging
