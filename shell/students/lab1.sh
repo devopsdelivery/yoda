9f88820cdddeff97cf1d0244f7805de293562334
ritasrodriguez <anarita.rodriguesformacao@gmail.com>
2024-09-18 11:54:43 +0100

    Update lab1.sh

---
+++ shell/students/lab1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "qual o teu nome?"
+read nome
+
+echo "qual o teu ultimo nome?" 
+read ultimo_nome
+
+echo "de onde es?"
+read onde_es
+
+echo "ola $nome $ultimo_nome de $onde_es. "
+
+
