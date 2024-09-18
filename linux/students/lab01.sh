3aaeeaef2c913c9de7750eac7f27becd052b6947
erica <ericamlmsoares@hotmail.com>
2024-09-18 10:53:43 +0100

    Added lab01 exercises

---
+++ linux/students/lab01.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+echo "What is your first name?"
+read FIRST_NAME
+
+echo "What is your last name?"
+read LAST_NAME
+
+echo "Where were you born?"
+read BIRTH_PLACE
+
+echo "Nice to meet you "$FIRST_NAME" "$LAST_NAME" from "$BIRTH_PLACE"."
