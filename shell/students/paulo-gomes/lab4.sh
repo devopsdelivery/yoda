8ce0e61c52f4980ec0952e83a49678529b17e8cb
root <root@DESKTOP-1E5R85O>
2024-09-18 12:10:23 +0100

    Commit_

---
+++ shell/students/paulo-gomes/lab4.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# threshold of disk
+THRESHOLD=80
+
+# disk usage
+USAGE=$(df / | grep / | awk '{ print $5 }' | sed 's/%//')
+
+# Check if the disk usage exceeds the threshold
+if [ "$USAGE" -gt "$THRESHOLD" ]; then
+  echo "Warning: Disk usage is above $THRESHOLD%. Current usage: $USAGE%"
+else
+  echo "Disk usage ok. Current usage: $USAGE%"
+fi
+
