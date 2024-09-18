b4411f5dbb3e88982d20e549293bfec0ae7e310a
Xavigit99 <xavier.ricarte@gmail.com>
2024-09-18 13:22:14 +0000

    Lab3 upgraded

--- shell/students/Xavier-Ricarte-Shell/lab3.sh
+++ shell/students/Xavier-Ricarte-Shell/lab3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mv /root/yoda/shell/students/Xavier-Ricarte-Shell/renamedir/file1.txt  /root/yoda/shell/students/Xavier-Ricarte-Shell/renamedir/backup_file1.txt
-mv /root/yoda/shell/students/Xavier-Ricarte-Shell/renamedir/file2.txt  /root/yoda/shell/students/Xavier-Ricarte-Shell/renamedir/backup_file2.txt
-mv /root/yoda/shell/students/Xavier-Ricarte-Shell/renamedir/file3.txt  /root/yoda/shell/students/Xavier-Ricarte-Shell/renamedir/backup_file3.txt
+for f in ".txt" do
+mv $f backp_$f
+done
 
