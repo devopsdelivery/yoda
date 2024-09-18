22cc37924166adcef7a00ab3f5ae76ccc58d52b7
root <root@DESKTOP-1E5R85O>
2024-09-18 15:05:03 +0100

    Commit_2024-09-18_15:05:03

--- shell/students/paulo-gomes/lab6.sh
+++ shell/students/paulo-gomes/lab6.sh
@@ -3,6 +3,20 @@
 #set -x;
 
 
+# Check if the correct number of arguments is provided
+if [ "$#" -ne 1 ]; then
+	echo "Usage: $0 <arg1(true|false)> "
+	exit 1
+else 
+   #if [ "$1" != "true" ] || [ "$1" != "false" ]; then 
+   if [ "$1" != "true" ] && [ "$1" != "false" ]; then
+        echo "Usage: $0 <arg1(true|false)> "
+	exit 1
+   fi	   
+       #VERBOSE
+       VERBOSE=$1
+fi
+
 # Log file path
 LOGFILE="/var/log/postgresql_service.log"
 
@@ -14,17 +28,31 @@ log_message() {
 
 # Check if PostgreSQL service is running
 if systemctl is-active --quiet postgresql; then
-    echo "PostgreSQL service is running."
+	if [ "$VERBOSE" == "true" ]; then
+		echo "PostgreSQL service is running."
+	fi
+    echo 0
+    exit 0
 else
-    echo "PostgreSQL service is not running. Starting service..."
+    if [ "$VERBOSE" == "true" ]; then
+	echo "PostgreSQL service is not running. Starting service..."
+    fi	
     log_message "PostgreSQL service is not running."
     log_message "Starting PostgreSQL service."
     sudo systemctl start postgresql
     if systemctl is-active --quiet postgresql; then
-	echo "PostgreSQL service started."
+        if [ "$VERBOSE" == "true" ]; then
+        	echo "PostgreSQL service started."
+        fi		    
 	log_message "PostgreSQL service is running."
+	echo 0
+	exit 0
     else
-	echo "PostgreSQL service cannot start."
+        if [ "$VERBOSE" == "true" ]; then
+               echo "PostgreSQL service cannot start."
+	fi
     	log_message "PostgreSQL service cannot start. Check logs for more info"
+	echo 1
+	exit 0
     fi
 fi
