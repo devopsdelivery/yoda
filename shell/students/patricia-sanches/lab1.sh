08ef737a63f214650cef7d27fb2294292494f128
PatNatixis <yodatheonewiththesparkle@gmail.com>
2024-09-18 13:47:17 +0100

    Update lab1.sh

---
+++ shell/students/patricia-sanches/lab1.sh
@@ -0,0 +1,8 @@
+echo "What is your first name?"
+read name
+echo "What is your last name?"
+read lastname
+echo "Where were you born?"
+read placeofbirth
+
+echo "Nice to meet you $name $lastname from $placeofbirth."
