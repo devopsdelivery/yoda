c6a68a6ed6b2bfc26b009fe211832479b0f03442
Allan Yoshioka <80413449+LstCenturion@users.noreply.github.com>
2024-09-18 11:23:06 +0100

    Lab2

---
+++ shell/students/Allan-Yoshioka/lab2.sh
@@ -0,0 +1,34 @@
+#! /bin/sh
+
+echo "Gime 3 numerbs!"
+echo "First"
+read first
+
+echo "Second" 
+read second 
+
+echo "Third" 
+read third 
+
+if [ $((first%2)) -eq 0 ]
+then
+    "First number is even"
+else 
+    "First number is odd"
+fi
+
+if [ $((second % 2)) -eq 0 ]
+then
+    "second number is even"
+else 
+    "second number is odd"
+fi
+
+
+if [ $((third % 2)) -eq 0 ]
+then
+    "Third number is even"
+else 
+    "third number is odd"
+fi
+
