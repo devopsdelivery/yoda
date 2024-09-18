19d909542894581b870da817911528b53a6fa620
Diogo Vieira <diogo.romero.paz@gmail.com>
2024-09-18 12:13:39 +0100

    overall corrections

--- lab2.sh
+++ lab2.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
 
-if [ "$#" -ne 3 ]; then
-	            echo "Error: You must provide exactly three numbers."
-		                    exit 1
+if [ "$#" -ne 3 ];
+then
+echo "Error: You must provide exactly three numbers"
+exit 1
 fi
 
 check_even_odd() {
-	        if [ $(( $1 % 2 )) -eq 0 ]; then
-			                echo "$1 is even"
-					                    else
-								                             echo "$1 is odd"
+if [ $(( $1 % 2 )) -eq 0 ];
+then
+echo "$1 is even"
+else
+echo "$1 is odd"
+fi
+}
+first_result=$(check_even_odd "$1")
+second_result=$(check_even_odd "$2")
+third_result=$(check_even_odd "$3")
 
-											             fi
-											     }
-
-											     first_result=$(check_even_odd "$1")
-											     second_result=$(check_even_odd "$2")
-											     third_result=$(check_even_odd "$3")
-
-											     echo "First number  $first_result"
-											     echo "Second number $second_result"
-											     echo "Third number $third_result"
+echo "First number  $first_result"
+echo "Second number $second_result"
+echo "Third number $third_result"
